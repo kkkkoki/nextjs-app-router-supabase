6a8becf683578a679ffc0f4da32ba89554252a46
kkkkoki <koki_inoue45@icloud.com>
2023-05-27 04:45:10 +0900

    auth画面のheight指定タイポの修正

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 export default function AuthPage() {
   return (
-    <main className="flex h-[calc(100vh-56px) flex-col items-center justify-center">
+    <main className="flex h-[calc(100vh-56px)] flex-col items-center justify-center">
       <Auth />
     </main>
   )
